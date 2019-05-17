3cc5ba1bc2971c2516bd4745cc767e86fe1d6817
Richard Donaldson <RichardWDonaldson@users.noreply.github.com>
2019-05-17 21:02:33 +0100

    Update ChartView.java

--- src/view/ChartView.java
+++ src/view/ChartView.java
@@ -1,43 +1,64 @@
 package view;
 
-import java.awt.EventQueue;
+import java.util.List;
 
-import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-public class ChartView {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
-	private JFrame frame;
+import model.Result;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ChartView window = new ChartView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class ChartView extends ApplicationFrame {
 
 	/**
-	 * Create the application.
+	 * 
 	 */
-	public ChartView() {
-		initialize();
-	}
+	private static final long serialVersionUID = 2326994307254984423L;
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+	public ChartView(String applicationTitle, String chartTitle, String categoryAxisLabel, String valueAxisLabel,
+			List<Result> results) {
+		super(applicationTitle);
 
+		JFreeChart lineChart = ChartFactory.createLineChart(chartTitle, categoryAxisLabel, valueAxisLabel,
+				standardErrorDataset(results), PlotOrientation.VERTICAL, true, true, false);
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+		setContentPane(chartPanel);
+
+	}
+	
+	public static void initialize(ChartView window) {
+
+		window.pack();
+		RefineryUtilities.centerFrameOnScreen(window);
+		window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		window.setVisible(true);
+
+	}
+	
+	public DefaultCategoryDataset standardErrorDataset(List<Result> results) {
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		int i = 1;
+
+		for (Result result : results) {
+
+			dataset.addValue(result.getMeanAbsoluteError(), "Mean", Integer.toString(i));
+			dataset.addValue(result.getRelativeAbsoluteError(), "Relative", Integer.toString(i));
+			dataset.addValue(result.getRootMeanSquaredError(), "Root Mean", Integer.toString(i));
+			dataset.addValue(result.getRootRelativeSquaredError(), "Root Relative", Integer.toString(i));
+
+			i++;
+
+		}
+		
+		return dataset;
+	}
+	
+	
 }
