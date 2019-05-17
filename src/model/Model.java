75be0ca48e58d7ec61c529ba233a3f97a58bdaba
Richard Donaldson <RichardWDonaldson@users.noreply.github.com>
2019-05-17 21:02:29 +0100

    Update Model.java

--- src/model/Model.java
+++ src/model/Model.java
@@ -8,7 +8,7 @@ public class Model {
 	
 	
 	
-	public void outputMLPResults(ChartSettings chartSettings, MLP settings, ArrayList<Result> results) {
+	public void outputMLPResults(ChartSettings chartSettings, Settings settings, ArrayList<Result> results) {
 
 		ChartView window = new ChartView(chartSettings.getTitle(), chartSettings.getSubTitle(),
 				chartSettings.getxAxisTitle(), chartSettings.getyAxisTitle(), results);
@@ -16,7 +16,7 @@ public class Model {
 
 	}
 	
-	public void outputRegressionResults(ChartSettings chartSettings, Regression settings,
+	public void outputRegressionResults(ChartSettings chartSettings, Settings settings,
 			ArrayList<Result> results) {
 		ChartView window = new ChartView(chartSettings.getTitle(), chartSettings.getSubTitle(),
 				chartSettings.getxAxisTitle(), chartSettings.getyAxisTitle(), results);
